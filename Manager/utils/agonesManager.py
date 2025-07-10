fa62de247917ac2ab94839a4447903e39d749091
Simon Lou <simon.lou.dev@proton.me>
2025-07-10 16:54:18 +0200

    Update agonesManager.py

--- Manager/utils/agonesManager.py
+++ Manager/utils/agonesManager.py
@@ -7,37 +7,16 @@ class AgonesManager:
         self.__headers = {'Content-type': 'application/json'}
 
     def send_health_check(self) -> None:
-        response = requests.post(self.__url + "/health", headers=self.__headers, json={})
-        if response.status_code == 200:
-            return
-        else:
-            raise Exception(f"Error fetching data: {response.status_code}")
+        pass
 
     def send_ready(self) -> None:
-        response = requests.post(self.__url + "/ready", headers=self.__headers, json={})
-        if response.status_code == 200:
-            return
-        else:
-            raise Exception(f"Error fetching data: {response.status_code}")
+        pass
 
     def send_allocate(self) -> None:
-        response = requests.post(self.__url + "/allocate", headers=self.__headers, json={})
-        if response.status_code == 200:
-            return
-        else:
-            raise Exception(f"Error fetching data: {response.status_code}")
+        pass
 
     def send_shutdown(self) -> None:
-        response = requests.post(self.__url + "/shutdown", headers=self.__headers, json={})
-        if response.status_code == 200:
-            return
-        else:
-            raise Exception(f"Error fetching data: {response.status_code}")
+        pass
 
     def get_info(self):
-        response = requests.get(self.__url + "/gameserver", headers=self.__headers)
-        if response.status_code == 200:
-            # Parse the response JSON into the JsonObject class
-            return response.json()
-        else:
-            raise Exception(f"Error fetching data: {response.status_code}")
+        pass
